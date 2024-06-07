9786edc24fea7bd6a603e19d936e55e59b02ee8a
gzmatte <117684932+gzmatte@users.noreply.github.com>
2024-06-07 16:44:04 -0300

    Add files via upload

---
+++ spotifylrm-i.bat
@@ -0,0 +1,16 @@
+@echo off
+color f0
+
+:: Line for changing spotx parameters, each parameter should be separated by a space      
+:: removed -confirm_spoti_recomended_over 
+set param=-v 1.2.13.661.ga588f749-4064 -block_update_on -adsections_off
+
+set url='https://raw.githubusercontent.com/SpotX-Official/spotx-official.github.io/main/run.ps1'
+set url2='https://spotx-official.github.io/run.ps1'
+set tls=[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12;
+
+%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe ^
+-Command %tls% $p='%param%'; """ & { $(try { iwr -useb %url% } catch { $p+= ' -m'; iwr -useb %url2% })} $p """" | iex
+
+timeout /t 2 > NUL 2>&1
+exit

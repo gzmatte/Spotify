28d1e49fe0d3d066cb2b2e2bbab291741a581b2f
gzmatte <117684932+gzmatte@users.noreply.github.com>
2024-06-07 16:32:27 -0300

    Add files via upload

---
+++ spotifylrm-u.bat
@@ -0,0 +1,46 @@
+@echo off
+color f0
+
+setlocal enabledelayedexpansion
+
+REM Reset permissions for Spotify Update folder
+if exist "%localappdata%\Spotify\Update" (
+    icacls "%localappdata%\Spotify\Update" /reset /T > NUL 2>&1
+)
+
+set actions=0
+
+REM Uninstall Spotify if it exists
+if exist "%appdata%\Spotify\Spotify.exe" (
+    start "" /wait "%appdata%\Spotify\Spotify.exe" /UNINSTALL /SILENT
+    set /a actions+=1
+)
+
+timeout /t 1 > NUL 2>&1
+
+REM Remove Spotify data folders
+for %%d in ("%appdata%\Spotify" "%localappdata%\Spotify") do (
+    if exist "%%d" (
+        rd /s/q "%%d" > NUL 2>&1
+        set /a actions+=1
+    )
+)
+
+REM Delete SpotifyUninstall.exe if it exists
+if exist "%temp%\SpotifyUninstall.exe" (
+    del /s /q  "%temp%\SpotifyUninstall.exe" > NUL 2>&1
+    set /a actions+=1
+)
+
+if !actions! == 0 (
+    echo.
+    echo Spotify is not installed or not found
+    echo.
+) else (
+    echo.
+    echo Spotify has been successfully uninstalled
+    echo.
+)
+
+timeout /t 2 /nobreak >nul
+exit
